3270b098293b2a9181f6b9d806e7a755d973a229
Ekanshi Agrawal <ekanshi258@gmail.com>
2020-04-27 02:01:52 +0530

    Pretty much done. Need to set colours well

---
+++ Bezier.h
@@ -0,0 +1,130 @@
+#ifndef BEZIER
+#define BEZIER
+
+#include <glm/glm.hpp>
+#include <vector>
+
+class Bezier{
+public:
+    float tRate;                       /**< Rate of change of t when drawing */
+    std::vector<glm::vec2>curve;       /**< Points on the curve */
+    std::vector<glm::vec2>controls;    /**< Control points */
+
+    /**
+     * Constructor
+     * @param tRate rate of increase of t
+     */
+    Bezier(float tRate)
+    {
+        this->tRate = tRate;
+    }
+
+    /**
+     * Linear Interpolation
+     * @param a first point
+     * @param b second point
+     * @param t weight/baricentre parameter
+     */
+    glm::vec2 Lerp(glm::vec2 a, glm::vec2 b, float t)
+    {
+        return glm::vec2((1-t)*(a.x) + t*(b.x), (1-t)*(a.y) + t*(b.y));
+    }
+
+    /**
+     * Make a point using the De Casteljau's Algorithm
+     * @param start starting control point
+     * @param end ending control point
+     * @param t parameter
+     * @return point on the curve
+     */
+    glm::vec2 DeCasteljau(int start, int end, float t)
+    {
+        std::vector <glm::vec2> temp; 
+        if(start == end) return this->controls[start];
+        int dif = end - start;
+        for(int i = start; i<=end; i++) temp.push_back(this->controls[i]);
+
+        for(int i=0; i<dif; i++)
+        {
+            // Linear imterpolations
+            for(int j=0; j<temp.size()-1; j++)
+            {
+                temp[j] = this->Lerp(temp[j], temp[j+1], t);
+            }
+            temp.pop_back();
+        }
+        return temp[0];
+    }
+
+    /**
+     * Add point and redraw curve using De Casteljau's algo
+     * @param pos position of new point
+     * @see DeCasteljau()
+     */
+    void newPoint(glm::vec2 pos)
+    {
+        this->controls.push_back(pos);
+        if(this->controls.size() == 1)
+        {
+            float t = 0.0;
+            while(t <= 1.0)
+            {
+                this->curve.push_back(this->controls[0]);
+                t += this->tRate;
+            }
+        }
+        else
+        {
+            int point = 0;
+            float t = 0.0;
+            int sz = this->controls.size();
+            while(t <= 1.0)
+            {
+                this->curve[point] = this->Lerp(this->curve[point], this->DeCasteljau(1, sz-1, t), t);
+                t += this->tRate;
+                point++;
+            }
+        }
+    }
+    /**
+     * Delete a control point and redraw using De Casteljau's algo
+     * @param id index of the point in the controls vector.
+     */
+    void deletePoint(int id)
+    {
+        if(id < this->controls.size() && id >=0)
+        {
+            this->controls.erase(this->controls.begin() + id);
+            this->curve.clear();
+            float t = 0.0;
+            while(t <= 1.0)
+            {
+                glm::vec2 point = this->DeCasteljau(0, this->controls.size()-1, t);
+                this->curve.push_back(point);
+                t += this->tRate;
+            }
+        }
+    }
+    /**
+     * Function to handle changina a control point and redraw
+     * @param id index of the point in the controls vector
+     * @param pos new position
+     */
+    void movePoint(int id, glm::vec2 pos)
+    {
+        if(id < this->controls.size())
+        {
+            this->controls[id] = pos;
+            this->curve.clear();
+            float t = 0.0;
+            while(t <= 1.0)
+            {
+                glm::vec2 point = this->DeCasteljau(0, this->controls.size()-1, t);
+                this->curve.push_back(point);
+                t += this->tRate;
+            }
+        }
+    }
+};
+
+#endif
---
+++ bezier1.cpp
@@ -0,0 +1,154 @@
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <iostream>
+#include <string>
+#include "Bezier.h"
+
+#define min(a,b) a>b?a:b
+#define EUCLIDEAN_DIST 51
+#define WIDTH 500
+#define HEIGHT 500
+float tRate = 0.1;
+
+int squareDist(glm::vec2 pos1, glm::vec2 pos2);
+int nearbyIndex(glm::vec2 point);
+void handleMouseClick(int click, int buttonStatus, int x, int y);
+void handleMouse(int x, int y);
+void resize(int w, int h);
+
+bool move;
+int movement;
+
+//! Bezier Curve object.
+Bezier curve(tRate);
+
+
+/**
+ * Draws the Bezier curve. 
+ * Serves as the display function for glut.
+ */
+void drawBezier()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINE_STRIP);
+    for( auto p: curve.controls)
+    {
+        glVertex2f(p.x, p.y);
+    }
+    glEnd();
+    glColor3f(1, 0, 1);
+    glBegin(GL_LINE_STRIP);
+    for( auto p: curve.curve)
+    {
+        glVertex2f(p.x, p.y);
+    }
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Assignment 3 - IS F311");
+    glClearColor(0.8, 0.8, 0.8, 0.8);
+    gluOrtho2D(0, WIDTH, 0, HEIGHT);
+    glutMotionFunc(handleMouse);
+    glutMouseFunc(handleMouseClick);
+    glutDisplayFunc(drawBezier);
+    glutReshapeFunc(resize);
+    glutMainLoop();
+
+    return 0;
+}
+
+/**
+ * Window resize handler
+ */
+void resize(int w, int h)
+{
+    glutReshapeWindow(WIDTH, HEIGHT);
+}
+/**
+ * Helper function: Squared Euclidean distance between two points
+ * @param pos1 first point
+ * @param pos2 second point
+ * @return squared distance
+ */
+int squareDist(glm::vec2 pos1, glm::vec2 pos2)
+{
+    return (pos1.x - pos2.x)*(pos1.x - pos2.x) + 
+                (pos1.y - pos2.y)*(pos1.y - pos2.y);
+}
+
+/**
+ * Method to find the index in Control Points of the nearest point within a radius
+ * @param point point from which distance is measured.
+ * @return index in controls.
+ */
+int nearbyIndex(glm::vec2 point)
+{
+    int mn = EUCLIDEAN_DIST+1;
+    int id = -1;
+    for(int i=0; i< curve.controls.size(); i++)
+    {
+        int curr = squareDist(point, curve.controls[i]);
+        if(mn > curr)
+        {
+            mn = curr;
+            id = i;
+        }
+    }
+    return id;
+}
+
+
+/**
+ * Handler function to add/delete/drag points. 
+ * @param click button being clicked
+ * @param buttonStatus status of the button (pressed/released)
+ * @param pos position of the cursor
+ */
+void handleMouseClick(int click, int buttonStatus, int x, int y)
+{
+     // drag using left button
+    if(click == GLUT_LEFT_BUTTON && buttonStatus == GLUT_DOWN)
+    {
+        movement = nearbyIndex(glm::vec2(x, HEIGHT-y));
+        if(movement != -1) move = true;
+    }
+    else if(click == GLUT_LEFT_BUTTON && buttonStatus == GLUT_UP)
+    {
+        move = false;
+        curve.movePoint(movement, glm::vec2(x, HEIGHT - y));
+    }
+    
+    // Add a point by middle clicking
+    else if(click == GLUT_MIDDLE_BUTTON && buttonStatus == GLUT_DOWN)
+    {
+        curve.newPoint(glm::vec2(x, HEIGHT - y));
+    }
+    
+    //Delete with right click
+    else if(click == GLUT_RIGHT_BUTTON && buttonStatus == GLUT_DOWN)
+    {
+        curve.deletePoint(nearbyIndex(glm::vec2(x, HEIGHT - y)));
+    }
+    glutPostRedisplay();
+}
+
+/**
+ * Handler function for mouse movements. Drags points when pressed.
+ * @param pos position of the cursor
+ */
+void handleMouse(int x, int y)
+{
+    if(move)
+    {
+        curve.movePoint(movement, glm::vec2(x, HEIGHT-y));
+        glutPostRedisplay();
+    }
+}
